05c302b1d665f9685d78be9652c673da6595eae4
NedosypI <irinanedosyp@gmx.de>
2022-07-13 11:31:28 +0200

    getSentenceArr() / Transformation I

--- script_01.js
+++ script_01.js
@@ -3,35 +3,61 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
-// let arr; 
-// // arr = new Array(); // Konstruktor 
-// arr = []; //Literal Schreibweise
-// arr =[2,7,11,4]; // 4 Elemente
+
+// let arr;
+// arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4]; // 4 Elemente
 // arr = [false,true,true];
 // arr = ["Ich","bin","Maxine","Mützerich"];
-// arr =[2,7,11,4,5,6,9,8,7]; 
+
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[3]); // // letzte Pos.
+// output(arr[arr.length-1]); // // letzte Pos.
 
 
-output(arr);
-output(arr.length); // Anzahl der Elemente 
-output(arr[0]); // Index 0 (1. Pos. im Array)
-output(arr[3]); // // letzte Pos.  
-output(arr[arr.length-1]); // // letzte Pos.  
+
+
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-//  output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","Maxine","Mützerich"));
-
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","der", "coole","Max","Mütze"));
+// more of the same!
+function getSentence(word1,word2,word3,word4,word5,word6) {
 
     const gap   = " ";
     const punct = ".";
     let str   =     word1 + gap +
                     word2 + gap +
                     word3 + gap +
-                    word4 + 
+                    word4 + gap +
+                    word5 + gap +
+                    word6 +
+                    punct;
+
+    return str;
+}
+
+/*** 01a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","Max","Mütze"]));
+function getSentenceArr(arr) {
+
+    const gap   = " ";
+    const punct = ".";
+    let str   =     arr[0] + gap +
+                    arr[1] + gap +
+                    arr[2] + gap +
+                    arr[3] +
                     punct;
 
     return str;
@@ -44,5 +70,3 @@ function getSentence(word1,word2,word3,word4) {
 function output(inputStr) {
     console.log(inputStr);
 }
-
-
